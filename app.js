dfe6f9f86b4d8bc17c53212b4433e16575eb8112
OleksandrVoloshyn <oleksandr.vvn@gmail.com>
2023-02-28 14:29:09 +0200

    done additional task less_1

--- app.js
+++ app.js
@@ -1,30 +1,62 @@
 const fs = require('node:fs')
-const path = require("node:path");
+const path = require("path");
 
 
-const workDir = 'papka'
-const names = ['First', 'Second', 'Third', 'Fourth', 'Fifth']
+const boysData = [
+    {name: "Sasha", gender: "male"},
+    {name: "Olya", gender: "female"},
+    {name: "Masha", gender: "female"},
+]
 
-fs.mkdir(path.join(__dirname, workDir), (err) => {
-    if (err) throw new Error(err.message)
-})
+const girlsData = [
+    {name: "Akakiy", gender: "male"},
+    {name: "Lana", gender: "female"},
+    {name: "Kokos", gender: "male"},
+]
 
-names.forEach(name => {
-    fs.mkdir(path.join(__dirname, workDir, `Dir-${name}`), (err) => {
+
+const createDir = (name) => {
+    fs.mkdir(path.join(__dirname, name), (err) => {
         if (err) throw new Error(err.message)
     })
-    fs.writeFile(path.join(__dirname, workDir, `File-${name}`), 'Hello World', (err) => {
-        if (err) throw new Error(err.message)
-    })
-})
+}
 
-fs.readdir(path.join(__dirname, workDir), (err, files) => {
-    files.forEach(file => {
-        fs.stat(path.join(__dirname, workDir, file), (err, stats) => {
+createDir('boys')
+createDir('girls')
+
+
+const writeFile = (data, dirName) => {
+    data.forEach(value => {
+        fs.writeFile(path.join(__dirname, dirName, `${value.name}.json`), JSON.stringify(value), (err) => {
             if (err) throw new Error(err.message)
-
-            if (stats.isFile()) return console.log(`FILE: ${file}`)
-            if (stats.isDirectory()) return console.log(`FOLDER: ${file}`)
         })
     })
-})
+}
+
+writeFile(boysData, 'boys')
+writeFile(girlsData, 'girls')
+
+
+const normalizeFolders = (readFrom, gender, writeTo) => {
+    fs.readdir(path.join(__dirname, readFrom), {withFileTypes: true}, (err, files) => {
+        if (err) throw new Error(err.message)
+
+        files.forEach(file => {
+            fs.readFile(path.join(__dirname, readFrom, file.name), (err, data) => {
+                if (err) throw new Error(err.message)
+
+                const user = JSON.parse(data.toString())
+                if (user.gender === gender) {
+                    fs.rename(
+                        path.join(__dirname, readFrom, file.name),
+                        path.join(__dirname, writeTo, file.name), (err) => {
+                            if (err) throw new Error(err.message)
+                        })
+                }
+            })
+        })
+    })
+}
+
+normalizeFolders('boys', 'female', 'girls')
+normalizeFolders('girls', 'male', 'boys')
